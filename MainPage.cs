4d2871006c151c32c61190382606372414c3ee2d
Andrew-Kiev <42326347+Andrew-Kiev@users.noreply.github.com>
2018-09-11 09:38:36 +0300

    Add MainPAge

---
+++ MainPage.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2.PageObject_iFinance
+{
+    public class MainPage
+    {
+        private IWebDriver driver;
+
+        public MainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public MainPage()
+        {
+        }
+
+        [FindsBy(How = How.XPath, Using = "(//tr[th/text()='USD']//span[contains(@class,'value')])[1]/span[1]")]
+        private decimal buyUSD;
+        [FindsBy(How = How.XPath, Using = "(//tr[th/text()='USD']//span[contains(@class,'value')])[1]/span[1]")]
+        private decimal sellUSD;
+        [FindsBy(How = How.XPath, Using = "(//tr[th/text()='USD']//span[contains(@class,'value')])[1]/span[1]")]
+        private decimal buyEUR;
+        [FindsBy(How = How.XPath, Using = "(//tr[th/text()='USD']//span[contains(@class,'value')])[1]/span[1]")]
+        private decimal sellEUR;
+        [FindsBy(How = How.XPath, Using = "(//tr[th/text()='USD']//span[contains(@class,'value')])[1]/span[1]")]
+        private decimal buyRUB;
+        [FindsBy(How = How.XPath, Using = "(//tr[th/text()='USD']//span[contains(@class,'value')])[1]/span[1]")]
+        private decimal sellRUB;
+
+        public static int MarjaUSD(decimal buyUSD, decimal sellUSD)
+        {
+            decimal marjaUSD;
+            marjaUSD = sellUSD - buyUSD;
+            return marjaUSD;
+        }
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("https://finance.i.ua/");
+        }
+    }
+
+
+}
